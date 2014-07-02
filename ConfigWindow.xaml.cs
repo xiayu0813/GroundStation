b8f713c45c62b78a07714db80e7c95c4d0ca4dc7
xiayu0813 <xia_yu0813@126.com>
2014-07-02 15:11:42 +0800

    建立基本框架

---
+++ ConfigWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO.Ports;
+
+namespace GroundStation
+{
+    /// <summary>
+    /// ConfigWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class ConfigWindow : Window
+    {
+        List<string> comList = new List<string>();
+        public ConfigWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btConfigOKCliked(object sender, RoutedEventArgs e)
+        {
+            Close();
+
+        }
+
+        private void btConfigCancelCliked(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            if(ports.Length > 0)
+            {
+                cbRecvPortName.ItemsSource = ports;
+                cbRecvPortName.SelectedIndex = 0;
+
+                cbSendPortName.ItemsSource = ports;
+                cbSendPortName.SelectedIndex = 1;
+            }
+
+        }
+    }
+}
---
+++ comm/SerialReceive.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace GroundStation
+{
+    public class SerialReceive
+    {//串口接收数据
+        static SerialPort RecvPort = new SerialPort(); //定义接收串口，不配置
+    }
+}
